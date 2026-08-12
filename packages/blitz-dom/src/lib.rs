ae85b08a72e24be1508c0d9756dc79ec049a3160
meh <no-reply@pathscale.com>
2026-08-13 06:45:48 +0700

    Pace slow CSS animations at 15 FPS

--- packages/blitz-dom/src/lib.rs
+++ packages/blitz-dom/src/lib.rs
@@ -84,7 +84,7 @@ pub use crate::node::{
 };
 pub use blitz_traits::node_id::NodeId;
 pub use config::{DocumentConfig, StyleThreading};
-pub use document::{BaseDocument, DocGuard, DocGuardMut, Document, PlainDocument};
+pub use document::{AnimationPacing, BaseDocument, DocGuard, DocGuardMut, Document, PlainDocument};
 pub use markup5ever::{
     LocalName, Namespace, NamespaceStaticSet, Prefix, PrefixStaticSet, QualName, local_name,
     namespace_prefix, namespace_url, ns,
